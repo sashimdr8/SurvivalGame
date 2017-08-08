f7d60c90601b2896a8dd2aa72a8f5084b1163bdd
Sashi <sashi.manandhar@braindigit.com>
2017-08-08 14:35:09 +0545

    mouse control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,16 @@ public class Player : MonoBehaviour {
 	public Rigidbody rbody;
 	private bool run;
 	// Use this for initialization
+
+	//Camera Script
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
+
+
 	void Start () {
 		anim = GetComponent<Animator> (); 
 		rbody = GetComponent<Rigidbody> ();
@@ -62,6 +72,13 @@ public class Player : MonoBehaviour {
 		float moveX = inputH * 100f * Time.deltaTime;
 		float moveZ = inputV * 800f * Time.deltaTime;
 
+
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+
 		if (moveZ <= 0f) {
 			moveX = 0f;
 		} else if (run) {
@@ -70,7 +87,22 @@ public class Player : MonoBehaviour {
 		}
 
 
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKeyDown("w") && Input.GetAxis ("Mouse X") != 0.0) {
+			moveX = yaw * 30f * Time.deltaTime;
+		}
 		rbody.velocity = new Vector3 (moveX, 0f, moveZ);
+
+
+
+
+		//Camera Script
+
+
+
+
+
+
+
 
 	}
 }
